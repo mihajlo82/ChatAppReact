2097fa44b159aa6c566737ca7cfddd225ebb695a
Mihajlo <dimitricmihajlo995@gmail.com>
2021-10-11 07:32:38 -0700

    initComm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { BulbFilled, NodeExpandOutlined } from '@ant-design/icons';
+import {useState} from 'react'
+import { ChatEngine, ChatFeed } from 'react-chat-engine';
+import './App.css'
+import ChadFeed  from './components/ChadFeed';
+import LoginForm from './components/LoginForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [isLogged, setIsLogged] = useState(true);
+     const logout = () => {
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+        setIsLogged(false);
+    }
+
+    if(!localStorage.getItem('username') || !isLogged) return <LoginForm />
+    return (
+        <>
+            <div>
+                <button
+                style={{
+                    margin: '4px',
+                    backgroundColor: "#7554a0",
+                    padding: "5px",
+                    color:'white',
+                    borderRadius: '4px',
+                    borderColor: 'transparent'
+                }}
+                 type="button" onClick={logout}>Log Out</button>
+            </div>
+            <ChatEngine
+                    height="100vh"
+                    projectID="fd93f42a-d17a-473a-a348-6311ca4e4919"
+                    userName={localStorage.getItem('username')}
+                    userSecret={localStorage.getItem('password')}
+                    renderChatFeed={(chatAppProps) => <ChadFeed {...chatAppProps} />}
+                /> 
+            </>
+    )
 }
 
-export default App;
+
+export default App
